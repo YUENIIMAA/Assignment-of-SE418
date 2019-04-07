8ab4083302fcaddad077df0acc0e8524c5f770c2
Richard Zhao <richard0410@sjtu.edu.cn>
2019-04-07 23:59:43 +0800

    Not Finished Yet

--- Homeworks/Huffman-Encoding/src/main/java/Main.java
+++ Homeworks/Huffman-Encoding/src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main2(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Input -c to compress or -d to decompress:");
         if (input.hasNext()) {
---
+++ Homeworks/Huffman-Encoding/src/main/java/service/Greeting.java
@@ -0,0 +1,22 @@
+package service;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        Compress cps = new Compress();
+
+        return content;
+    }
+}
