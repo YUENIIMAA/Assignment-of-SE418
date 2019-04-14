7ea0465048db0388f711570186ca0e9533c47513
Richard <richard0410@sjtu.edu.cn>
2019-04-14 17:00:16 +0800

    Spring security added

---
+++ Homework/Huffman-Encoding/src/main/java/huffman_encoding/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package huffman_encoding;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("Richard").password("{noop}Password").roles("ADMIN").and()
+                .withUser("TA").password(("{noop}TeacherAssistent")).roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .fullyAuthenticated()
+                .and().httpBasic();
+        http.csrf().disable();
+    }
+
+}
