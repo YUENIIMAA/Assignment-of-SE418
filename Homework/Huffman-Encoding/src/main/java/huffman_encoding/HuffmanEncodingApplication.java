6d7b699576fa7eda08daf71bcb886d86053a2644
Richard <richard0410@sjtu.edu.cn>
2019-04-14 23:47:06 +0800

    MVC and Actuator added

---
+++ Homework/Huffman-Encoding/src/main/java/huffman_encoding/HuffmanEncodingApplication.java
@@ -0,0 +1,13 @@
+package huffman_encoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuffmanEncodingApplication {
+
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(HuffmanEncodingApplication.class, args);
+	}
+
+}
---
+++ Homework/Huffman-Encoding/src/main/java/huffman_encoding/ResponseController.java
@@ -0,0 +1,13 @@
+package huffman_encoding;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResponseController {
+    @RequestMapping("/encode")
+    public Encoding compress(@RequestParam(value="text", defaultValue="Huffman Encoding") String text) {
+        return new Encoding(text);
+    }
+}
